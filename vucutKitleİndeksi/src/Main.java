6e660404187b0eb06e55e04b678e1a95ee2cae3e
Buğra Çelik <bqracelik06@hotmail.com>
2022-11-26 10:28:38 +0200

    Vücut Kitle İndeksi
    Vücut kitle indeksini hesaplar.

---
+++ vucutKitleİndeksi/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double boy,kilo,kitleİndeks;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz:");
+        boy = inp.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz:");
+        kilo = inp.nextDouble();
+        kitleİndeks = kilo / (boy * boy) ;
+
+        System.out.println("Vücut kitle indeksiniz :" +kitleİndeks);
+    }
+}
